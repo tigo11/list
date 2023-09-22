2039bf1307a56556b57126692284290e65f81ec3
tigo11 <91616660+tigo11@users.noreply.github.com>
2023-09-22 21:08:55 +0300

    Add files via upload

---
+++ list/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	list "example/list/storage"
+	"fmt"
+)
+
+func main() {
+	l := list.NewList()
+
+	l.Add(4)
+	l.Add(8)
+	l.Add(34)
+	l.Add(8)
+	l.Add(7)
+	l.Add(79)
+	l.Add(117)
+	l.Add(4)
+	l.Add(56)
+	l.Add(12)
+
+	fmt.Println("Длина листа:", l.Len())
+
+	values, ok := l.GetAll()
+	if ok {
+		fmt.Println("Элементы листа:", values)
+	} else {
+		fmt.Println("Пусто")
+	}
+
+	l.RemoveByIndex(2)
+	fmt.Println("Длина листа после удаления элемента по индексу:", l.Len())
+
+	values, ok = l.GetAll()
+	if ok {
+		fmt.Println("Элементы в листе после удаления:", values)
+	} else {
+		fmt.Println("Пусто")
+	}
+	l.RemoveAllByValue(11)
+	l.RemoveByValue(5)
+	values, ok = l.GetAll()
+	if ok {
+		fmt.Println("Элементы в листе после удаления по значению:", values)
+	} else {
+		fmt.Println("Пусто")
+	}
+
+	fmt.Println(l.GetByIndex(3))
+	fmt.Println(l.GetByValue(27))
+	fmt.Println(l.GetAllByValue(2))
+
+	fmt.Println("Вывод элементов листа:")
+	l.Print()
+
+	l.Clear()
+	fmt.Println("Длина листа после очистки:", l.Len())
+}
